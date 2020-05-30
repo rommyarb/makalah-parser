bc0a499c6cc1ea8bf82833f8abab5523b38481e9
Rommy <arbantas@gmail.com>
2020-05-30 07:23:43 +0700

    current update

---
+++ js/app.js
@@ -0,0 +1,336 @@
+// This site's Engine v1.0.0
+// by @rommyarb
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    finished: false,
+    msg: "Bambang",
+    processing: false,
+    currentFilename: "",
+    statusLog: "",
+    xmlFiles: [],
+    errors: [],
+    nodes: [
+      {
+        author: "Bambang",
+        publishedYear: "2020",
+        citedBy: [
+          { nama: "ohohoh" },
+          { nama: "ohohoh" },
+          { nama: "ohohoh" },
+          { nama: "ohohoh" },
+          { nama: "ohohoh" },
+        ],
+      },
+      {
+        author: "Bambang 2",
+        publishedYear: "2020",
+        citedBy: [{ nama: "ohohoh" }, { nama: "ohohoh" }],
+      },
+
+      {
+        author: "Bambang 3",
+        publishedYear: "2018",
+        citedBy: [{ nama: "ohohoh" }, { nama: "ohohoh" }],
+      },
+    ],
+  },
+  computed: {
+    nodesJson() {
+      return JSON.stringify(this.nodes)
+    },
+  },
+  methods: {
+    initProcessFullText() {
+      this.startProcessFullText(0)
+    },
+    startProcessFullText(i, retry = false) {
+      this.processFullText(i, retry)
+    },
+    async processFullText(i, retry) {
+      var url = "https://grobid.rommyarb.dev/api/processFulltextDocument"
+      var form = new FormData()
+      var files = this.$refs.fileinput.files
+      var file = files[i]
+      if (file) {
+        if (!retry) {
+          this.currentFilename = file.name
+          // this.statusLog = "Parsing " + (i + 1) + " of " + files.length + " files...";
+        }
+        this.addBeforeUnloadEvent()
+        form.append("input", file)
+        this.processing = true
+        try {
+          var that = this
+          var r = await axios.post(url, form, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+            onUploadProgress: function (progressEvent) {
+              var completed = Math.round(
+                (progressEvent.loaded / progressEvent.total) * 100
+              )
+              if (completed < 100) {
+                that.statusLog = "Uploading " + completed + " %"
+              } else {
+                that.statusLog =
+                  "Parsing " + (i + 1) + " of " + files.length + " files..."
+              }
+            },
+          })
+
+          var xmlData = r.data
+
+          /////////////////////////////////////////////////////////////////////////
+          if (parser.validate(xmlData)) {
+            var options = {
+              ignoreAttributes: false,
+            }
+            var jsonObj = parser.parse(xmlData, options)
+
+            // TITLE
+            var title =
+              jsonObj["TEI"]["teiHeader"]["fileDesc"]["titleStmt"]["title"][
+                "#text"
+              ]
+            var authors =
+              jsonObj["TEI"]["teiHeader"]["fileDesc"]["sourceDesc"][
+                "biblStruct"
+              ]["analytic"]["author"]
+            // console.log("TITLE:", title)
+
+            // PUBLISHED YEAR
+            var publishedYear = jsonObj["TEI"]["teiHeader"]["fileDesc"][
+              "sourceDesc"
+            ]["biblStruct"]["monogr"]["imprint"]["date"]["@_when"]
+              ? jsonObj["TEI"]["teiHeader"]["fileDesc"]["sourceDesc"][
+                  "biblStruct"
+                ]["monogr"]["imprint"]["date"]["@_when"]
+              : null
+
+            // AUTHOR(S)
+            var theAuthor = ""
+            if (Array.isArray(authors)) {
+              for (var [j, author] of authors.entries()) {
+                if (author) {
+                  var roleName = author["persName"]
+                    ? author["persName"]["roleName"]
+                    : null
+                  var surname = author["persName"]
+                    ? author["persName"]["surname"]
+                    : null
+                  var fullname = (roleName ? roleName + " " : "") + surname
+                  if (fullname != undefined && fullname != "undefined") {
+                    authors[j] = fullname
+                  }
+                } else {
+                  authors[j] = null
+                }
+              }
+              theAuthor = authors.join(" & ").replace(/\s+/g, " ").trim()
+            } else {
+              var author = authors
+              var roleName = author["persName"]
+                ? author["persName"]["roleName"]
+                : null
+              var surname = author["persName"]
+                ? author["persName"]["surname"]
+                : null
+              var fullname = (roleName ? roleName + " " : "") + surname
+              theAuthor = fullname.replace(/\s+/g, " ").trim()
+            }
+
+            var references = []
+            var refDiv = jsonObj["TEI"]["text"]["back"]["div"]
+            if (Array.isArray(refDiv)) {
+              references = refDiv.find(
+                (item) => item["@_type"] == "references"
+              )["listBibl"]["biblStruct"]
+            } else {
+              references = refDiv["listBibl"]["biblStruct"]
+            }
+
+            if (references) {
+              // START ITERATION HERE
+
+              for (var ref of references) {
+                // Ref Title
+                var titleRef = ref["monogr"]["title"]["#text"]
+
+                // Ref Author
+                var authorsRef = ref["analytic"]
+                if (authorsRef) {
+                  authorsRef = ref["analytic"]["author"]
+                } else {
+                  authorsRef = ref["monogr"]["author"]
+                }
+                var theAuthorRef = "" // 1
+                if (Array.isArray(authorsRef)) {
+                  for (var [j, author] of authorsRef.entries()) {
+                    if (author) {
+                      var roleName = author["persName"]
+                        ? author["persName"]["roleName"]
+                        : null
+                      var surname = author["persName"]
+                        ? author["persName"]["surname"]
+                        : null
+                      var fullname = (roleName ? roleName + " " : "") + surname
+                      if (fullname != undefined && fullname != "undefined") {
+                        authorsRef[j] = fullname
+                      }
+                    } else {
+                      authorsRef[j] = null
+                    }
+                  }
+                  // console.log("authorsRef:", authorsRef)
+                  theAuthorRef = authorsRef
+                    .join(" & ")
+                    .replace(/\s+/g, " ")
+                    .trim()
+                } else {
+                  if (authorsRef) {
+                    var author = authorsRef
+                    var roleName = author["persName"]
+                      ? author["persName"]["roleName"]
+                      : null
+                    var surname = author["persName"]
+                      ? author["persName"]["surname"]
+                      : null
+                    var fullname = (roleName ? roleName + " " : "") + surname
+                    theAuthorRef = fullname.replace(/\s+/g, " ").trim()
+                  } else {
+                    theAuthorRef = null
+                  }
+                }
+
+                if (theAuthorRef) {
+                  // Ref Published Year
+                  var publishedYearRef = ref["monogr"]["imprint"]["date"]
+                    ? ref["monogr"]["imprint"]["date"]["@_when"]
+                    : null
+
+                  var node = {
+                    title: titleRef,
+                    author: theAuthorRef,
+                    publishedYear: publishedYearRef,
+                    citedBy: [
+                      {
+                        title: title,
+                        author: theAuthor,
+                        publishedYear: publishedYear,
+                      },
+                    ],
+                  }
+
+                  // console.log("Filename:", file.name)
+                  // console.log("NODE:", node)
+
+                  var checkNode = this.nodes.find((mainNode) => {
+                    if (
+                      mainNode.author === node.author &&
+                      mainNode.publishedYear === node.publishedYear
+                    ) {
+                      return true
+                    } else {
+                      return false
+                    }
+                  })
+
+                  if (checkNode) {
+                    // console.warn("checkNode exists, adding to citedBy....")
+                    // add to citedBy
+                    var checkCitedBy = checkNode.citedBy.find(
+                      (item) =>
+                        item.author == theAuthor &&
+                        item.publishedYear == publishedYear
+                    )
+                    if (checkCitedBy) {
+                      // console.log("citedBy already exist")
+                    } else {
+                      // console.log("add to citedBy")
+                      checkNode.citedBy = [
+                        ...checkNode.citedBy,
+                        {
+                          title: title,
+                          author: theAuthor,
+                          publishedYear: publishedYear,
+                        },
+                      ]
+                    }
+                  } else {
+                    // console.log("checkNode doesn't exists, pushing new node")
+                    // push to nodes
+                    this.nodes.push(node)
+                  }
+                } else {
+                  // console.warn("The ref author is null.")
+                }
+              }
+            }
+          }
+
+          updateGraph(this.nodes)
+
+          /////////////////////////////////////////////////////////////////////////
+
+          if (i < files.length - 1) {
+            // console.log("next please...")
+            // console.log(
+            // "========================================================================================="
+            // )
+            this.startProcessFullText(i + 1)
+          } else {
+            // console.log("🏁🏁 FINISHED 🏁🏁")
+            this.processing = false
+            this.finished = true
+
+            // save to json
+            var blob = new Blob([JSON.stringify(this.nodes)], {
+              type: "application/json; charset=UTF-8",
+            })
+            saveAs(blob, "hasil_parse_" + files.length + "_file.json")
+          }
+        } catch (e) {
+          console.error("ERROR:", e)
+          var status = e.response ? e.response.status : null
+          if (status) {
+            // skip this file
+            this.statusLog = "File ERROR. Skipping..."
+            this.errors.push(file)
+            var that = this
+            setTimeout(function () {
+              if (i < files.length - 1) {
+                that.startProcessFullText(i + 1)
+              } else {
+                that.processing = false
+                // console.log(that.xmlFiles)
+              }
+            }, 500)
+          } else {
+            // try again
+            // console.log("trying again")
+            this.statusLog =
+              "[Retry] Processing " +
+              (i + 1) +
+              " of " +
+              files.length +
+              " files..."
+            this.startProcessFullText(i, true)
+          }
+        }
+      } else {
+        alert("No files selected.")
+      }
+    },
+    onBeforeUnload(e) {
+      e.returnValue = "In progress."
+    },
+    addBeforeUnloadEvent() {
+      addEventListener("beforeunload", this.onBeforeUnload)
+    },
+    removeBeforeUnloadEvent() {
+      removeEventListener("beforeunload", this.onBeforeUnload)
+    },
+  },
+})
---
+++ js/graph.js
@@ -0,0 +1,212 @@
+var dataURL = "https://rommyarb.dev/burivira/graph/data.json"
+var showNameAndYear = true
+var showSize = true
+var labels
+var graph = []
+
+var svg = d3
+  .select("div#chart")
+  .append("svg")
+  .attr("width", "100%")
+  .attr("height", "100%")
+  .call(
+    d3.zoom().on("zoom", function () {
+      svg.attr("transform", d3.event.transform)
+    })
+  )
+  .append("g")
+
+var color = d3.scaleOrdinal(d3.schemeCategory20c)
+// var nodeRadius = 20; // for collide force
+var width = window.innerWidth
+var height = window.innerHeight
+
+var simulation = d3
+  .forceSimulation()
+  .force(
+    "link",
+    d3
+      .forceLink()
+      .id(function (d) {
+        return d.id
+      })
+      .distance(80)
+  )
+  .force("charge", d3.forceManyBody().strength(-2))
+  .force("center", d3.forceCenter(width / 2, height / 2))
+  .force(
+    "collide",
+    d3
+      .forceCollide()
+      .radius(function (d) {
+        return d.size + 0.5
+      })
+      .iterations(4)
+  )
+// .force("x", d3.forceX().strength(0.00001))
+// .force("y", d3.forceY().strength(0.00001));
+
+fetch(dataURL)
+  .then((r) => r.json())
+  .then((r) => {
+    graph = r
+    simulation.nodes(graph.nodes)
+    simulation.force("link").links(graph.links)
+
+    var link = svg
+      .append("g")
+      .attr("class", "link")
+      .selectAll("line")
+      .data(graph.links)
+      .enter()
+      .append("line")
+
+    var node = svg
+      .append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(graph.nodes)
+      .enter()
+      .append("circle")
+
+      //Setting node radius by size value. If 'size' key doesn't exist, set radius to 9
+      .attr("r", function (d) {
+        if (d.hasOwnProperty("size")) {
+          return d.size * 1
+        } else {
+          return 9
+        }
+      })
+      //Colors by 'size' value
+      .style("fill", function (d) {
+        return color(d.size)
+      })
+      .call(
+        d3
+          .drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended)
+      )
+
+    node
+      .append("svg:title")
+      .attr("dx", 12)
+      .attr("dy", ".35em")
+      .text(function (d) {
+        return d.id
+      })
+
+    labels = svg
+      .append("g")
+      .attr("class", "label")
+      .selectAll("text")
+      .data(graph.nodes)
+      .enter()
+      .append("text")
+      .attr("dx", 6)
+      .attr("dy", ".35em")
+      .style("font-size", 10)
+      .text(function (d) {
+        return d.label
+      })
+
+    simulation.nodes(graph.nodes).on("tick", ticked)
+
+    simulation.force("link").links(graph.links)
+
+    function ticked() {
+      link
+        .attr("x1", function (d) {
+          return d.source.x
+        })
+        .attr("y1", function (d) {
+          return d.source.y
+        })
+        .attr("x2", function (d) {
+          return d.target.x
+        })
+        .attr("y2", function (d) {
+          return d.target.y
+        })
+
+      node
+        .attr("cx", function (d) {
+          return d.x
+        })
+        .attr("cy", function (d) {
+          return d.y
+        })
+      labels
+        .attr("x", function (d) {
+          return d.x
+        })
+        .attr("y", function (d) {
+          return d.y
+        })
+    }
+  })
+
+setTimeout(function () {
+  simulation.force("charge", d3.forceManyBody().strength(0))
+  simulation.alpha(1).restart()
+}, 2000)
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+  d.fx = d.x
+  d.fy = d.y
+}
+
+function dragged(d) {
+  d.fx = d3.event.x
+  d.fy = d3.event.y
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0)
+  d.fx = null
+  d.fy = null
+}
+
+function toggleNameAndYear() {
+  graph.nodes.forEach((node) => {
+    if (showNameAndYear) {
+      var split = node.label.split(" (")
+      node.label = split[0]
+    }
+  })
+  renderLabel()
+}
+function toggleSize() {
+  graph.nodes.forEach((node) => {
+    if (showSize) {
+      var split = node.label.split(" (")
+      var sizeOnly = split[1].replace("Size: ", "").replace(")", "")
+      node.label = sizeOnly
+    }
+  })
+  renderLabel()
+}
+
+function renderLabel() {
+  svg.selectAll("text").remove()
+  labels = svg
+    .append("g")
+    .attr("class", "label")
+    .selectAll("text")
+    .data(graph.nodes)
+    .enter()
+    .append("text")
+    .attr("dx", -6)
+    .attr("dy", ".35em")
+    .style("font-size", 10)
+    .text(function (d) {
+      return d.label
+    })
+
+  var elements = document.getElementsByClassName("control-btn")
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0])
+  }
+}
